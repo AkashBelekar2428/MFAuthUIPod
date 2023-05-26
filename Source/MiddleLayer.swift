3707637888c35bba68bb2fef90a91a5f925313cc
akashbelekar2428 <akashbelekar108@gmail.com>
2023-05-26 12:13:26 +0530

    changes

--- Source/MiddleLayer.swift
+++ Source/MiddleLayer.swift
@@ -9,7 +9,7 @@
 
 import Foundation
 import UIKit
-import TAAuthenticationUI
+//import TAAuthenticationUI
 import ObjectMapper
 import SVProgressHUD
 
--- Source/WSHelper.swift
+++ Source/WSHelper.swift
@@ -9,6 +9,7 @@
 
 import Foundation
 import ObjectMapper
+import Alamofire
 
 enum responseObject
 {
